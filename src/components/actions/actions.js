4353348ea4bcee978c06009950b8a3ae8f0b5bed
wYuJian <569365020@qq.com>
2018-06-26 11:47:49 +0800

    redux

---
+++ src/components/actions/actions.js
@@ -0,0 +1,28 @@
+
+// actions.js
+/*
+ * action 类型
+ */
+export const ADD_TODO = 'ADD_TODO';
+export const TOGGLE_TODO = 'TOGGLE_TODO ';
+export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER ';
+/*
+ * action 类型
+ */
+export const VisibilityFilters  = {
+  SHOW_ALL:'SHOW_ALL',
+  SHOW_COMPLETED:'SHOW_COMPLETED',
+  SHOW_ACTIVE:'SHOW_ACTIVE'
+}
+/*
+ * action 类型
+ */
+export function addTodo(text) {
+  return { type: ADD_TODO,text}
+}
+export function toggleTodo(index) {
+  return { type: TOGGLE_TODO,index}
+}
+export function setVisibilityFilter(filter) {
+  return { type: SET_VISIBILITY_FILTER, filter}
+}
--- src/components/component.js
+++ src/components/component.js
@@ -2,6 +2,35 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import MediaQuery from "react-responsive";
+// app.js
+import {
+  addTodo,
+  toggleTodo,
+  setVisibilityFilter,
+  VisibilityFilters,
+} from './actions/actions';
+import {createStore} from 'redux'
+import todoAPP from './reducers/reducers'
+let store = createStore(todoAPP)
+console.log(store.getState());
+
+// 每次 state 更新时，打印日志
+// 注意 subscribe() 返回一个函数用来注销监听器
+const unsubscribe = store.subscribe(() =>
+  console.log(store.getState())
+)
+// 发起一系列 action
+store.dispatch(addTodo('Learn about reducers'));
+store.dispatch(addTodo('Learn about actions'));
+store.dispatch(addTodo('Learn about reducers'));
+store.dispatch(addTodo('Learn about store'));
+store.dispatch(toggleTodo(0));
+store.dispatch(toggleTodo(1));
+store.dispatch(setVisibilityFilter(VisibilityFilters.SHOW_COMPLETED));
+
+// 停止监听 state 更新
+unsubscribe();
+
 
 import PC from "./pc/router.js";
 import Mobile from "./mobile/index.js";
